f3824127bb7d72d1f2716308c7c3ca5a2d9fcc56
Saul Almanzar <almanzarsaul@icloud.com>
2023-02-24 09:55:26 -0400

    Implement remove from cart

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { menuArray } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 const menu = document.getElementById("menu");
 const checkout = document.getElementById("checkout-items");
 const totalPriceEl = document.getElementById("total-price");
@@ -12,6 +13,15 @@ menu.innerHTML = renderMenu(menuArray);
 menu.addEventListener("click", function (event) {
   if (event.target.classList.contains("add-btn")) {
     handleAddToCart(event.target.dataset.name, event.target.dataset.price);
+  }
+});
+
+checkout.addEventListener("click", function (event) {
+  if (event.target.dataset.removeId) {
+    handleRemoveFromCart(
+      event.target.dataset.removeId,
+      event.target.dataset.removePrice
+    );
   }
 });
 
@@ -44,13 +54,20 @@ function renderMenu(menu) {
 }
 
 function handleAddToCart(name, price) {
-  checkoutItems.push({ name, price });
+  checkoutItems.push({ id: uuidv4(), name, price });
   totalPrice += Number(price);
   checkout.innerHTML = renderCheckout(checkoutItems);
   totalPriceEl.innerText = totalPrice;
 }
 
-function handleRemoveFromCart() {}
+function handleRemoveFromCart(id, price) {
+  checkoutItems = checkoutItems.filter(function (item) {
+    return item.id !== id;
+  });
+  totalPrice -= Number(price);
+  checkout.innerHTML = renderCheckout(checkoutItems);
+  totalPriceEl.innerText = totalPrice;
+}
 
 function renderCheckout(items) {
   let checkoutHtml = "";
@@ -58,7 +75,7 @@ function renderCheckout(items) {
     checkoutHtml += `
         <div class="checkout-item">
             <p>${item.name}</p>
-            <a href="">remove</a>
+            <a href="#" data-remove-id="${item.id}" data-remove-price="${item.price}">remove</a>
             <p class="align-right">$${item.price}</p>
         </div>`;
   });
